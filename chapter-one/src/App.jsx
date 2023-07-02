0b8378841ace434c16a1ad10efceadc55263f274
Ramtin <ramtinkamanian82@gmail.com>
2023-07-02 19:55:37 +0330

    added login component

--- chapter-one/src/App.jsx
+++ chapter-one/src/App.jsx
@@ -1,11 +1,11 @@
 
 import './App.css';
+import Login from './Components/Login';
 import SignUp from './Components/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <SignUp/>
     </div>
   );
 }
